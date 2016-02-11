2141f536b2190dee861494d743180ad107c8b387
Andy Kulis <akulis@users.noreply.github.com>
2016-02-11 14:53:59 -0600

    Update branding.js

--- app/js/directives/branding.js
+++ app/js/directives/branding.js
@@ -6,14 +6,6 @@ four51.app.directive('branding', function() {
     return obj;
 });
 
-four51.app.directive('brandingAC', function() {
-    var obj = {
-        restrict: 'E',
-        templateUrl: 'partials/brandingAC.html'
-    }
-    return obj;
-});
-
 four51.app.directive('homemessage', function() {
 	var obj = {
 		restrict: 'E',
