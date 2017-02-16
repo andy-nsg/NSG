8b9ada3b834fa74999bb93266f502344c66faa63
ak <kid-a@Andrews-MacBook-Pro.local>
2017-02-15 22:02:34 -0600

    update alerts how

--- app/js/directives/alertShow.js
+++ app/js/directives/alertShow.js
@@ -8,7 +8,7 @@ four51.app.directive('alertShow', ['$animate', function($animate) {
                 if (show) {
                     $animate.enabled(true);
                     $animate.removeClass(element, 'ng-hide');
-                    $timeout($animate.addClass(element, 'ng-hide'), 10000); // Gives us a timeout of 10 seconds $ak
+                    //$timeout($animate.addClass(element, 'ng-hide'), 10000); // Gives us a timeout of 10 seconds $ak
                     $animate.enabled(false);
                 }
             });
