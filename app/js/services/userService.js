9c3edbd0e97f42582439ec7932ece2e60a8957fc
ak <kid-a@Andrews-MacBook-Pro.local>
2017-02-17 10:18:45 -0600

    update userEditCtrl.js & userService.js

--- app/js/services/userService.js
+++ app/js/services/userService.js
@@ -16,7 +16,9 @@ four51.app.factory('User', ['$q', '$rootScope', '$resource', '$451', 'Security',
 			if (f.ControlType == 'File' && f.File && f.File.Url.indexOf('auth') == -1)
 				f.File.Url += "&auth=" + Security.auth();
 	    });
-	    u.Company.POIDMask = u.Company.POIDMask.toUpperCase();
+	    if(u.Company.POIDMask){
+			u.Company.POIDMask = u.Company.POIDMask.toUpperCase();
+		}
     }
 
 	var _refresh = function() {
@@ -65,7 +67,12 @@ four51.app.factory('User', ['$q', '$rootScope', '$resource', '$451', 'Security',
 		store.clear();
 		$resource($451.api('login')).save(credentials).$promise.then(
 			function(u) {
-				_then(success, u);
+				if(credentials.CurrentOrderID){
+					_setorder(credentials.CurrentOrderID,success,error);
+				}
+				else{
+					_then(success, u);
+				}
 			},
 			function(ex) {
 				if (angular.isFunction(error)) {
