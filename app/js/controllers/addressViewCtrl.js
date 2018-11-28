65948b66ece536f6f32bda107d76e3eea981111c
andy <akulis@users.noreply.github.com>
2018-11-28 11:59:30 -0600

    Re-add files

---
+++ app/js/controllers/addressViewCtrl.js
@@ -0,0 +1,13 @@
+four51.app.controller("AddressViewCtrl", [
+	"$scope",
+	"$routeParams",
+	"Address",
+	function($scope, $routeParams, Address) {
+		$routeParams.id
+			? Address.get($routeParams.id, function(add) {
+					$scope.address = add;
+				})
+			: ($scope.address = { Country: "US" });
+		$scope.return = "/addresses";
+	}
+]);
