a10454b43f7484c2fddf926ba981f8b2d6c216e3
andy <akulis@users.noreply.github.com>
2020-02-05 13:59:12 -0600

    February 451 Update

--- app/js/services/errorService.js
+++ app/js/services/errorService.js
@@ -1,23 +1,30 @@
-four51.app.factory("Error", function() {
-	var defineError = function(ex) {
-		var obj = {
-			Message: ex.data.Message || ex.Message || "",
-			Detail: ex.data.ExceptionMessage || ex.ExceptionMessage || ex.message,
-			Code: { text: ex.data.ExceptionType || ex.ExceptionType || "" },
-			StackTrace: ex.data.StackTrace || ex.StackTrace || ""
-		};
-		obj.Code.text = obj.Code.text
-			.replace("Four51.Framework.", "")
-			.replace("DBExceptions+", "");
+four51.app.factory('Error', function() {
+  var defineError = function(ex) {
+    var obj = {
+      Message: ex.data.Message || ex.Message || '',
+      Detail: ex.data.ExceptionMessage || ex.ExceptionMessage || ex.message,
+      Code: {
+        text: ex.data.ExceptionType || ex.ExceptionType || ''
+      },
+      StackTrace: ex.data.StackTrace || ex.StackTrace || ''
+    }
+    obj.Code.text = obj.Code.text.replace('Four51.Framework.', '').replace('DBExceptions+', '');
 
-		obj.Code.is = function(code) {
-			return obj.Code.text.indexOf(code) > -1;
-		};
+    if (obj.Message.indexOf("There are three distinct password security levels") !== -1) {
+      obj.Code.text = "PasswordSecurityException";
+    }
+    if (obj.Message.indexOf("Account information for that email address cannot be located") !== -1) {
+      obj.Code.text = "EmailNotFoundException";
+    }
 
-		return obj;
-	};
+    obj.Code.is = function(code) {
+      return obj.Code.text.indexOf(code) > -1;
+    }
 
-	return {
-		format: defineError
-	};
+    return obj;
+  }
+
+  return {
+    format: defineError
+  }
 });
