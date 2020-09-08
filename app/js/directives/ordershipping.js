c6b4daa752212cc44175accd45456bb672a38086
andy <akulis@users.noreply.github.com>
2020-09-08 14:21:51 -0500

    Update ordershipping.js

--- app/js/directives/ordershipping.js
+++ app/js/directives/ordershipping.js
@@ -173,7 +173,7 @@ four51.app.directive("ordershipping", [
           });
 
           var setShipping = function(shippers) {
-            var lower48 = ['AL', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
+            var lower48 = ['AL', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
             var usOrder = false;
             angular.forEach($scope.shipaddresses, function(address) {
               if (address.ID === $scope.currentOrder.ShipAddressID) {
