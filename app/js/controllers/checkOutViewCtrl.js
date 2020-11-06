a8255215ae9bb8f757d036a94f06ff67e9ae562a
andy <akulis@users.noreply.github.com>
2020-11-06 13:08:55 -0600

    Update checkOutViewCtrl.js

--- app/js/controllers/checkOutViewCtrl.js
+++ app/js/controllers/checkOutViewCtrl.js
@@ -1,16 +1,4 @@
-four51.app.controller('CheckOutViewCtrl', [
-  '$scope',
-  '$routeParams',
-  '$location',
-  '$filter',
-  '$rootScope',
-  '$451',
-  'User',
-  'Order',
-  'OrderConfig',
-  'FavoriteOrder',
-  'AddressList',
-  'GoogleAnalytics',
+four51.app.controller('CheckOutViewCtrl', ['$scope', '$routeParams', '$location', '$filter', '$rootScope', '$451', 'User', 'Order', 'OrderConfig', 'FavoriteOrder', 'AddressList', 'GoogleAnalytics',
   function($scope, $routeParams, $location, $filter, $rootScope, $451, User, Order, OrderConfig, FavoriteOrder, AddressList, GoogleAnalytics) {
     $scope.errorSection = 'open';
 
@@ -26,32 +14,33 @@ four51.app.controller('CheckOutViewCtrl', [
     }
 
     $scope.hasOrderConfig = OrderConfig.hasConfig($scope.currentOrder, $scope.user);
-    $scope.checkOutSection = $scope.hasOrderConfig
-      ? 'order'
-      : 'shipping';
+    $scope.checkOutSection = $scope.hasOrderConfig ? 'order' : 'shipping';
 
     function submitOrder() {
       $scope.displayLoadingIndicator = true;
       $scope.submitClicked = true;
       $scope.errorMessage = null;
-      Order.submit($scope.currentOrder, function(data) {
-        if ($scope.user.Company.GoogleAnalyticsCode) {
-          GoogleAnalytics.ecommerce(data, $scope.user);
-        }
-        $scope.user.CurrentOrderID = null;
-        User.save($scope.user, function(data) {
-          $scope.user = data;
+      Order.submit($scope.currentOrder,
+        function(data) {
+          if ($scope.user.Company.GoogleAnalyticsCode) {
+            GoogleAnalytics.ecommerce(data, $scope.user);
+          }
+          $scope.user.CurrentOrderID = null;
+          User.save($scope.user, function(data) {
+            $scope.user = data;
+            $scope.displayLoadingIndicator = false;
+          });
+          $scope.currentOrder = null;
+          $location.path('/order/new/' + data.ID);
+        },
+        function(ex) {
+          $scope.submitClicked = false;
+          $scope.errorMessage = ex.Message;
           $scope.displayLoadingIndicator = false;
-        });
-        $scope.currentOrder = null;
-        $location.path('/order/new/' + data.ID);
-      }, function(ex) {
-        $scope.submitClicked = false;
-        $scope.errorMessage = ex.Message;
-        $scope.displayLoadingIndicator = false;
-        $scope.shippingUpdatingIndicator = false;
-        $scope.shippingFetchIndicator = false;
-      });
+          $scope.shippingUpdatingIndicator = false;
+          $scope.shippingFetchIndicator = false;
+        }
+      );
     };
 
     $scope.$watch('currentOrder.CostCenter', function() {
@@ -59,8 +48,7 @@ four51.app.controller('CheckOutViewCtrl', [
     });
 
     $scope.$watch('currentOrder.LineItems', function(item) {
-      if (!item) 
-        return;
+      if (!item) return;
       if ($scope.user.ShipMethod && $scope.user.ShipMethod.DefaultShipperAccountNumber) {
         angular.forEach($scope.currentOrder.LineItems, function(li) {
           li.ShipAccount = $scope.user.ShipMethod.DefaultShipperAccountNumber;
@@ -69,8 +57,7 @@ four51.app.controller('CheckOutViewCtrl', [
     });
 
     $scope.$watch('currentOrder.LineItems[0].ShipAccount', function(val) {
-      if (!val) 
-        return;
+      if (!val) return;
       if (!$scope.currentOrder.IsMultipleShip()) {
         angular.forEach($scope.currentOrder.LineItems, function(li) {
           li.ShipAccount = val;
@@ -84,58 +71,62 @@ four51.app.controller('CheckOutViewCtrl', [
       $scope.actionMessage = null;
       var auto = $scope.currentOrder.autoID;
       var cache = angular.copy($scope.currentOrder);
-      Order.save($scope.currentOrder, function(data) {
-        $scope.currentOrder = data;
-        if (cache.CreditCard) {
-          $scope.currentOrder.CreditCard = cache.CreditCard;
+      Order.save($scope.currentOrder,
+        function(data) {
+          $scope.currentOrder = data;
+          if (cache.CreditCard) {
+            $scope.currentOrder.CreditCard = cache.CreditCard;
+          }
+          if (cache.ExternalOrderDetailRecipients) {
+            $scope.currentOrder.ExternalOrderDetailRecipients = cache.ExternalOrderDetailRecipients;
+          }
+          if (auto) {
+            $scope.currentOrder.autoID = true;
+            $scope.currentOrder.ExternalID = 'auto';
+          }
+          $scope.displayLoadingIndicator = false;
+          if (callback) callback($scope.currentOrder);
+          else {
+            $scope.actionMessage = "Your changes have been saved";
+          }
+        },
+        function(ex) {
+          $scope.currentOrder.ExternalID = null;
+          $scope.errorMessage = ex.Message;
+          $scope.displayLoadingIndicator = false;
+          $scope.shippingUpdatingIndicator = false;
+          $scope.shippingFetchIndicator = false;
         }
-        if (cache.ExternalOrderDetailRecipients) {
-          $scope.currentOrder.ExternalOrderDetailRecipients = cache.ExternalOrderDetailRecipients;
-        }
-        if (auto) {
-          $scope.currentOrder.autoID = true;
-          $scope.currentOrder.ExternalID = 'auto';
-        }
-        $scope.displayLoadingIndicator = false;
-        if (callback) 
-          callback($scope.currentOrder);
-        else {
-          $scope.actionMessage = "Your changes have been saved";
-        }
-      }, function(ex) {
-        $scope.currentOrder.ExternalID = null;
-        $scope.errorMessage = ex.Message;
-        $scope.displayLoadingIndicator = false;
-        $scope.shippingUpdatingIndicator = false;
-        $scope.shippingFetchIndicator = false;
-      });
+      );
     };
 
     $scope.continueShopping = function() {
-      if (confirm('Do you want to save changes to your order before continuing?') == true) 
+      if (confirm('Do you want to save changes to your order before continuing?') == true)
         saveChanges(function() {
           $location.path('catalog')
         });
-      else 
+      else
         $location.path('catalog');
-      }
-    ;
+    };
 
     $scope.cancelOrder = function() {
       if (confirm('Are you sure you wish to cancel your order?') == true) {
         $scope.displayLoadingIndicator = true;
-        Order.delete($scope.currentOrder, function() {
-          $scope.user.CurrentOrderID = null;
-          $scope.currentOrder = null;
-          User.save($scope.user, function(data) {
-            $scope.user = data;
+        Order.delete($scope.currentOrder,
+          function() {
+            $scope.user.CurrentOrderID = null;
+            $scope.currentOrder = null;
+            User.save($scope.user, function(data) {
+              $scope.user = data;
+              $scope.displayLoadingIndicator = false;
+              $location.path('catalog');
+            });
+          },
+          function(ex) {
+            $scope.actionMessage = ex.Message;
             $scope.displayLoadingIndicator = false;
-            $location.path('catalog');
-          });
-        }, function(ex) {
-          $scope.actionMessage = ex.Message;
-          $scope.displayLoadingIndicator = false;
-        });
+          }
+        );
       }
     };
 
@@ -144,6 +135,7 @@ four51.app.controller('CheckOutViewCtrl', [
     };
 
     $scope.submitOrder = function() {
+      $scope.submitClicked = true;
       saveChanges(function(data) {
         submitOrder();
       });
