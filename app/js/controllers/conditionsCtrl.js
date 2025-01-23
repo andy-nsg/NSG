e57de071989aede8a99b47e2ceaf3f726ff7ca12
Andy K <andy.kulis@ibvi.org>
2025-01-22 18:37:03 -0600

    Refactor controllers to use single quotes and improve code formatting

--- app/js/controllers/conditionsCtrl.js
+++ app/js/controllers/conditionsCtrl.js
@@ -1,13 +1,9 @@
-four51.app.controller("ConditionsCtrl", [
-	"$scope",
-	"$location",
-	"User",
-	function($scope, $location, User) {
-		$scope.accept = function() {
-			$scope.user.AcceptConditions = true;
-			User.save($scope.user, function() {
-				$location.path("catalog");
-			});
-		};
-	}
-]);
+four51.app.controller('ConditionsCtrl', ['$scope', '$location', 'User',
+  function ($scope, $location, User) {
+    $scope.accept = function() {
+      $scope.user.AcceptConditions = true;
+      User.save($scope.user, function() {
+        $location.path('catalog');
+      });
+    }
+  }]);
